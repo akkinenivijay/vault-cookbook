f76432919910406981560b5000603cf84e3bcfd0
Vijay Akkineni <vakkineni@kabbage.com>
2018-01-02 23:19:06 -0500

    tls fixes

--- libraries/vault_config.rb
+++ libraries/vault_config.rb
@@ -77,8 +77,9 @@ module VaultCookbook
           config_keeps.include?(k.to_sym)
         end
         # listener
-        listener_keeps = %i(address cluster_address proxy_protocol_behavior proxy_protocol_authorized_addrs)
-        tls_params = %i(tls_cert_file tls_key_file tls_min_version tls_cipher_suites tls_prefer_server_cipher_suites tls_require_and_verify_client_cert tls_client_ca_file)
+        listener_keeps = tls? ? %i(address cluster_address proxy_protocol_behavior proxy_protocol_authorized_addrs tls_cert_file tls_key_file tls_min_version tls_cipher_suites tls_prefer_server_cipher_suites tls_require_and_verify_client_cert tls_client_ca_file) : %i(address cluster_address proxy_protocol_behavior proxy_protocol_authorized_addrs)
+        # listener_keeps = %i(address cluster_address proxy_protocol_behavior proxy_protocol_authorized_addrs)
+        # tls_params = %i(tls_cert_file tls_key_file tls_min_version tls_cipher_suites tls_prefer_server_cipher_suites tls_require_and_verify_client_cert tls_client_ca_file)
         listener_options = to_hash.keep_if do |k, _|
           listener_keeps.include?(k.to_sym)
         end.merge(tls_disable: tls_disable.to_s)
